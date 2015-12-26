d2448a82775b4de9b0615deef9c39093350007c8
Tae Hyung Kim <thkim1011@hotmail.com>
2015-12-26 14:16:22 -0800

    Changed Readme

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-# README
-## This is the README for your extension "vs-latex" 
-You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+# VS LaTeX Extension - README
+## Note
+I created this originally for my own use as a student so I could easily compile and view LaTeX file on VS Code. However, I learned node.js and typescript very briefly in order to make this, so use this at your own risk. If there is a lot of interest in this project, I may think about extending if further. Enjoy!
 
-* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
+## Usage
+There are two ways to compile to PDF:
+* Go to the command palette and type "LaTeX: Build LaTeX to PDF"
+* Use the button "Build to PDF" in the status bar.
+* Make use of settings.json (currently the latex binaries must be added to PATH).
+Note that when you are using the command from the command palette, your active window must be the LaTeX file.
 
-### For more information
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+## Plans
+* Add a button for enabling latexmk.
+* Make support for Asymptote for use with LaTeX.
+* Find a better way to open PDF files.
 
-** Enjoy!**
+### More Information
+* [Github](https://github.com/thkim1011/vs-latex.git
--- src/extension.ts
+++ src/extension.ts
@@ -8,20 +8,21 @@ export function activate(context: ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	let build = new Build();
+	let controller = new Controller(build);
 	let out = window.createOutputChannel("PdfTeX Output");
 	let open = require("open");
 	let viewer;
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	var disposable = commands.registerCommand('extension.sayHello', () => {
+	
+	var disposable = commands.registerCommand('extension.buildPDF', () => {
 		// The code you place here will be executed every time your command is executed
 		let path = require('path');
 		commands.executeCommand("workbench.action.files.save");
 		let child = require("child_process");
 		out.show(2);
 		let doc = window.activeTextEditor.document;
-		viewer.kill();
 		let pathToDir = path.dirname(doc.fileName);
 		var pdflatex = child.spawnSync("pdflatex", 
 		[doc.fileName, 
@@ -59,8 +60,7 @@ export function activate(context: ExtensionContext) {
 	});
 	
 	context.subscriptions.push(disposable);
-	
-	build.buildToPDF();
+	context.subscriptions.push(controller);
 }
 
 // this method is called when your extension is deactivated
@@ -70,16 +70,45 @@ export function deactivate() {
 class Build {
 	private _toPDF: StatusBarItem;
 	
-	public buildToPDF() {
+	public buildToPDFButton() {
 		if (!this._toPDF) { 
             this._toPDF = window.createStatusBarItem(StatusBarAlignment.Left); 
 			this._toPDF.text = "$(beaker) Build to PDF";
-			this._toPDF.command = "extension.sayHello";
-			this._toPDF.show(); 
+			this._toPDF.command = "extension.buildPDF";
+		}
+		if (window.activeTextEditor.document.languageId != "latex") {
+			this._toPDF.hide();
+		}
+		else {
+			this._toPDF.show();
 		}
 	}
 	
 	public dispose() {
 		this._toPDF.dispose();
 	}
+}
+
+class Controller {
+
+    private _build: Build;
+    private _disposable: Disposable;
+
+    constructor(build: Build) {
+        this._build = build;
+        this._build.buildToPDFButton();
+        let subscriptions: Disposable[] = [];
+        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+        this._build.buildToPDFButton();
+        this._disposable = Disposable.from(...subscriptions);
+	}
+
+    dispose() {
+        this._disposable.dispose();
+    }
+
+    private _onEvent() {
+        this._build.buildToPDFButton();
+    }
 }
